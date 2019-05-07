32c976f8001099a8f3d4a8801a44571df1b70fec
Casper Väresmaa <casper.varesmaa@gmail.com>
2019-05-08 00:10:48 +0300

    adds ability to pass semantic spesific props from UISchema
    pass props to SemanticUI from UISchema with following template:
    "ui:options": {
          "semantic": {
          "size": "huge",
            "icon": "search"
          }
        }

--- src/components/widgets/BaseInput.js
+++ src/components/widgets/BaseInput.js
@@ -38,6 +38,7 @@ function BaseInput(props) {
       onChange={!readOnly && _onChange}
       onBlur={onBlur && (event => onBlur(inputProps.id, event.target.value))}
       onFocus={onFocus && (event => onFocus(inputProps.id, event.target.value))}
+      {...options.semantic}
     />
   );
 }
