ebf11baad8a392e8812eb25e5ecfaac19f0d0c4c
Casper Väresmaa <casper.varesmaa@gmail.com>
2019-05-07 21:51:56 +0300

    Changes ErrorList.js to use SemanticUI components

--- src/components/ErrorList.js
+++ src/components/ErrorList.js
@@ -1,21 +1,25 @@
 import React from "react";
+import { List, ListItem, Divider, Message } from "semantic-ui-react";
 
 export default function ErrorList(props) {
   const { errors } = props;
   return (
-    <div className="panel panel-danger errors">
-      <div className="panel-heading">
-        <h3 className="panel-title">Errors</h3>
-      </div>
-      <ul className="list-group">
-        {errors.map((error, i) => {
-          return (
-            <li key={i} className="list-group-item text-danger">
-              {error.stack}
-            </li>
-          );
-        })}
-      </ul>
+    <div>
+      <Message error>
+        <div className="panel-heading">
+          <h3 className="panel-title">Errors</h3>
+        </div>
+        <List>
+          {errors.map((error, i) => {
+            return (
+              <ListItem key={i}>
+                <span className="error">{error.stack}</span>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Message>
+      <Divider hidden />
     </div>
   );
 }
